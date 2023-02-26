453dbb2133cde9547658e4533fadfabb6ff416e8
isso-719 <i@kazukiisogai.net>
2023-02-26 18:28:13 +0900

    add empty tests

---
+++ pkg/usecase/message_usecase_test.go
@@ -0,0 +1,7 @@
+package usecase
+
+import "testing"
+
+func Test_CreateMessage(t *testing.T) {}
+
+func Test_GetAllMessages(t *testing.T) {}
---
+++ pkg/usecase/room_usecase_test.go
@@ -0,0 +1,11 @@
+package usecase
+
+import "testing"
+
+func Test_generateRandomToken(t *testing.T) {}
+
+func Test_CreateRoom(t *testing.T) {}
+
+func Test_FindRoom(t *testing.T) {}
+
+func Test_JoinRoom(t *testing.T) {}
