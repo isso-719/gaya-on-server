a2d7b94d115bfc54350c92357236b57f06a5bf42
isso-719 <i@kazukiisogai.net>
2023-03-07 22:26:47 +0900

    MySQL charset utf8mb3_general_ci に対応

--- pkg/infra/db_conn.go
+++ pkg/infra/db_conn.go
@@ -29,7 +29,7 @@ func NewSQLConnector() *SQLConnector {
 }
 
 func sqlConnInfo(sqlInfo config.SQLInfo) string {
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true&loc=Local&charset=utf8mb4",
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true&loc=Local",
 		sqlInfo.SQLUser,
 		sqlInfo.SQLPassword,
 		sqlInfo.SQLAddress,
